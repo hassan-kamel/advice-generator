08b4f5ab2769e1bc35f7dde2d1eda479cc71538f
hassan mohamed kamel <hassankamel755@gmail.com>
2022-10-30 02:12:49 +0200

    Finishedd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,41 @@
+import { useEffect, useState } from 'react';
+import Dice from './components/Dice';
+import PatternDividerDesk from './components/PatternDividerDesk';
+import PatternDividerMob from './components/PatternDividerMob';
+
 function App() {
-  return <div className='App'></div>;
+  const [advice, setAdvice] = useState({});
+
+  const getAvice = () => {
+    fetch('https://api.adviceslip.com/advice')
+      .then((res) => res.json())
+      .then((adv) => {
+        setAdvice(adv.slip);
+      });
+  };
+  useEffect(() => {
+    getAvice();
+  }, []);
+  return (
+    <div className='app'>
+      <span>advice #{advice.id}</span>
+      <p>
+        <sup>❝ </sup>
+        {advice.advice}
+        <sup> ❞</sup>
+      </p>
+      <div className='divider'>
+        {window.innerWidth < 680 ? (
+          <PatternDividerMob />
+        ) : (
+          <PatternDividerDesk />
+        )}
+      </div>
+      <button className='dice' onClick={getAvice}>
+        <Dice />
+      </button>
+    </div>
+  );
 }
 
 export default App;
